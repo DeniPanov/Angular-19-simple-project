ead47384e821a8d1596a6c74907d1e1223fe8a59
DeniPanov <inedvonap@gmail.com>
2025-03-23 17:15:06 +0200

    feat: add tasks.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,22 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { NgFor } from '@angular/common';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgFor, HeaderComponent, UserComponent ],
+  imports: [NgFor, HeaderComponent, UserComponent, TasksComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  username = '';
 
   onSelectUser(id: string) {
-    console.log("Selected user for id " + id);
+    if (id) {
+      this.username = this.users.find(u => u.id === id)?.name || '';
+    }
   }
 }
